6cfcb2dc421417baeb39010b917e0a01cae2743b
gunt <4261629+gunt@users.noreply.github.com>
2019-09-16 23:39:02 -0600

    Update flags.js

--- js/flags.js
+++ js/flags.js
@@ -1,3 +1,9 @@
+// displaying the data
+// Now, we need to create the structure of our list in order 
+// to display the data and for that we're going to create all the 
+// elements that we need (ul, li, headings, etc) inside of a function 
+// and we'll insert them into the results div we declared in our HTML.
+
 let countries;
 
 const fetchCountries = async () => {
@@ -14,4 +20,10 @@ const showCountries = async () => {
 
     const ul = document.createElement('ul');
     ul.classList.add('countries');
+
+    countries.forEach(country => {
+        //creating the structure
+        const li = document.createElement('li');
+        li.classList.add('country-item');
+    }
 }
