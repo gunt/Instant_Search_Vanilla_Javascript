9ba5fa93d6655b4d8866f0d4eb47c59c8bf5bcf9
gunt <4261629+gunt@users.noreply.github.com>
2019-09-17 11:11:49 -0600

    calling fetchCountries function
    call our fetchCountries function here because we want to loop over the countries in order to create the corresponding elements:

--- js/flags.js
+++ js/flags.js
@@ -25,5 +25,31 @@ const showCountries = async () => {
         //creating the structure
         const li = document.createElement('li');
         li.classList.add('country-item');
+
+        const country_flag = document.createElement('img');
+        // Setting the image source
+        country_flag.src = country.flag;
+        country_flag.classList.add('country-flag');
+
+        const country_name = document.createElement('h3');
+        country_name.innterText = country.name;
+        country_name.classList.add('country-name');
+
+        const country_info = document.createElement('div');
+        country_info.classList.add('country-info');
+
+        const country_population = document.createElement('h2');
+        country_population.innerText = numberWithComas(country.population);
+        country_population.classList.add('country-population');
+
+        const country_population_text = document.createElement('h5');
+        country_population_text.innerText = 'Population';
+        country_population_text.classList.add('country-population-text');
+
+        country_info.appendChild(country_population);
+        country_info.appendChild(country_population_text);
+
+        li.appendChild(country_flag);
+        li.appendChild
     }
 }
